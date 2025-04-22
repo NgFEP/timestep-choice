80ab0a5b648c8769a84a982552ab752ee1259630
Saikat Pal <48545587+saikat0003@users.noreply.github.com>
2025-04-22 15:47:21 -0400

    Update run_analysis.sh

--- DATA/c-md/camp/hmr_on_shake_on/run/results/run_analysis.sh
+++ DATA/c-md/camp/hmr_on_shake_on/run/results/run_analysis.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-#python step_1_extract_1.py drift NS Etot_1
-#python step_8_extract.py start end 5 image Etot_1
-
 for energy in Etot_1 
 do
   python step_1_extract_1.py drift NS "$energy"
